f2d6cfc224c8fd66cec2508a64ccb5653687ada7
Pierre HENRY <pierre.henry@code99.fr>
2023-11-01 12:59:33 +0100

    Récupération des donnés et mise en forme accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,26 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+export function photographerTemplate({
+  id,
+  name,
+  portrait,
+  tagline,
+  city,
+  country,
+  price,
+}) {
+  const picture = `assets/photographers/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    article.classList.add("card");
+    article.innerHTML = `
+        <a class="link" href="photographer.html?id=${id}">
+            <img class="portrait" src="${picture}" alt="${name}">
+            <h2 class="name">${name}</h2>
+        </a>
+        <p class="location">${city}, ${country}</p>
+        <p class="tagline">${tagline}</p>
+        <p class="price">${price}&nbsp;&euro;&nbsp;/&nbsp;jour</p>`;
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
