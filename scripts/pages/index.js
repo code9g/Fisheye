81930d349f98ac8e391c10229909d34a997c6203
Pierre HENRY <pierre.henry@code99.fr>
2023-11-21 08:33:01 +0100

    Retrait de la constante QS_*

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,9 +1,8 @@
-import { QS_PHOTOGRAPHERS } from "../utils/consts.js";
 import { getPhotographers } from "../utils/data.js";
 import { photographerCardTemplate } from "./../templates/photographerCard.js";
 
 async function displayData(photographers) {
-  const photographersSection = document.querySelector(QS_PHOTOGRAPHERS);
+  const photographersSection = document.querySelector(".photographers");
 
   for (const photographer of photographers) {
     photographersSection.appendChild(photographerCardTemplate(photographer));
--- scripts/utils/consts.js
+++ scripts/utils/consts.js
@@ -3,5 +3,3 @@ export const PATH_PHOTOGRAPHERS = `${PATH_ASSETS}/photographers`;
 export const PATH_MEDIA = `${PATH_ASSETS}/media`;
 
 export const URL_DATA = `data/photographers.json`;
-
-export const QS_PHOTOGRAPHERS = ".photographers";
